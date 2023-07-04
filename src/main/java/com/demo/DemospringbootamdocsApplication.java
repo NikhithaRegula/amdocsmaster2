83cce30fc2b50d31e919c4dd32e6183d1f1245be
NikhithaRegula <regulanikitha@gmail.com>
2023-07-04 14:19:51 +0530

    first commit

---
+++ src/main/java/com/demo/DemospringbootamdocsApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootamdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringbootamdocsApplication.class, args);
+	}
+
+}
